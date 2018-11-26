bf99e556a6a45e9f842e068a826beca4da39d4dc
Shuwen Yue <syue@princeton.edu>
2018-11-26 00:39:33 -0500

    added bound the root function with unit testing

--- newton.py
+++ newton.py
@@ -13,69 +13,77 @@ class Newton(object):
 
     """
     
-    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6,Df=None,r=None):
-        """Parameters:
+    def __init__(self, f, tol=1.e-6, maxiter=20, dx=1.e-6, Df=None, max_radius=None):
+        """Parameters: f(function), tol(tolerance), maxiter, dx(step size), 
+        Df(analytical jacobian, optional), max_radius(max radius from x0
         
-        f: the function whose roots we seek. Can be scalar- or
-        vector-valued. If the latter, must return an object of the
-        same "shape" as its input x
-
-        tol, maxiter: iterate until ||f(x)|| < tol or until you
-        perform maxiter iterations, whichever happens first
-
-        dx: step size for computing approximate Jacobian
-
         """
         self._f = f
         self._tol = tol
         self._maxiter = maxiter
         self._dx = dx
 
+        # set Df if specified, then analytical jacobian is calculated
+        # otherwise approximate jacobian is calculated
+        if Df != None:
+            self._Df = Df
+        else: self._Df = 0
+
+        # set max_radius if specified, otherwise default max_radius=5
+        if max_radius != None:
+            self._max_radius = max_radius
+        else:
+            self._max_radius = 5
+
+
+
     def solve(self, x0):
         """Determine a solution of f(x) = 0, using Newton's method, starting
-        from initial guess x0. x0 must be scalar or 1D-array-like. If
-        x0 is scalar, return a scalar result; if x0 is 1D, return a 1D
-        numpy array.
+        from initial guess x0.
 
         """
-        # NOTE: no need to check whether x0 is scalar or vector. All
-        # the functions/methods invoked inside solve() return "the
-        # right thing" when x0 is scalar.
         x = x0
         for i in range(self._maxiter):
-            fx = self._f(x)
-            # linalg.norm works fine on scalar inputs
-            if np.linalg.norm(fx) < self._tol: # when f(x) magnitude is approx. 0, exit iteration loop
+
+            # evaluate f at x
+            fx = self._f(x) 
+
+            # if fx is close enough to 0, exit loop and return solution x
+            if np.linalg.norm(fx) < self._tol:
                 return x
-            x = self.step(x, fx) # take a step, given x and f(x)
 
-            # Bound the root: allow user to specify a radius r around initial guess x0
-            # raises exception if x0 is far away from x
+            # iterate another step
+            x = self.step(x, fx)
 
-        return x # return the x value that results in f(x)=0
+            # Bound the root: allow user to specify a radius max_radius around initial guess x0
+            # returns 0 (with error message) if computed root is far away from x0
+            if np.linalg.norm(x-x0) > self._max_radius: #pick a suitable  max_radius, default = 10
+                print("computed root is above maximum radius threshold")
+                return 0
+
+        # Error if not converged after max interations
+        if np.linalg.norm(fx) > self._tol:
+            print( "Did not converge in maximum number of iterations. Change initial guess or increase number of iterations or tolerance")
+            return 0
+
+        return x
 
     def step(self, x, fx=None):
         """Take a single step of a Newton method, starting from x. If the
         argument fx is provided, assumes fx = f(x).
 
         """
-        # fx can be given, or not
-        if fx is None: # if fx not given, then fx just evaluated at x
+        Df_x = F.approximateJacobian(self._f, x, self._dx)
+
+        # Determine if analytic form or approximate form of Jacobian is to be used
+        if fx is None:
             fx = self._f(x)
 
-        # find the derivative of f at x
-        Df_x = F.approximateJacobian(self._f, x, self._dx)
-        # linalg.solve(A,B) returns the matrix solution to AX = B, so
-        # it gives (A^{-1}) B. np.matrix() promotes scalars to 1x1
-        # matrices.
+        # Df_x^-1 f(x) is solved
         h = np.linalg.solve(np.matrix(Df_x), np.matrix(fx))
-        # Suppose x was a scalar. At this point, h is a 1x1 matrix. If
-        # we want to return a scalar value for our next guess, we need
-        # to re-scalarize h before combining it with our previous
-        # x. The function np.asscalar() will act on a numpy array or
-        # matrix that has only a single data element inside and return
-        # that element as a scalar.
+
+        # if x is a scalar, change h to scalar before 
         if np.isscalar(x):
             h = np.asscalar(h)
 
-        return x + h
+        return x - h
--- testNewton.py
+++ testNewton.py
@@ -2,31 +2,48 @@
 
 import unittest
 import numpy as np
-
+import functions as F
 import newton
+import math
 
 class TestNewton(unittest.TestCase):
-    def testLinear(self):
-        # Just so you see it at least once, this is the lambda keyword
-        # in Python, which allows you to create anonymous functions
-        # "on the fly". As I commented in testFunctions.py, you can
-        # define regular functions inside other
-        # functions/methods. lambda expressions are just syntactic
-        # sugar for that.  In other words, the line below is
-        # *completely equivalent* under the hood to:
-        #
-        # def f(x):
-        #     return 3.0*x + 6.0
-        #
-        # No difference.
-        f = lambda x : 3.0*x + 6.0
 
-        # Setting maxiter to 2 b/c we're guessing the actual root
+    # find root of simple linear function
+    def testLinear(self):
+        f = lambda x : 3.0*x + 6.0
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
-        x = solver.solve(-2.0)
-        # Equality should be exact if we supply *the* root, ergo
-        # assertEqual rather than assertAlmostEqual
+        x0 = -2.0
+        x = solver.solve(x0)
         self.assertEqual(x, -2.0)
+
+    # check root of nonlinear function
+    def testNonLinear(self):
+        f = lambda x : x*x-1
+        solver = newton.Newton(f, tol=1.e-15, maxiter=30)
+        x0 = 0.5
+        x = solver.solve(x0)    
+        self.assertEqual(x, 1.0)
+
+    # check 'bound the root' if x step is > max_radius
+    def testBoundtheRoot(self):
+        f = lambda x: 3.0*x + 6.0
+        solver = newton.Newton(f, tol=1.e-15, maxiter=2,max_radius=2)
+        x0 = 100
+        x = solver.solve(x0)
+        self.assertEqual(x, 0) # 0 return value for 'bound the root' error
+
+    # check max iterations
+    def testMaxIterations(self):
+        f = F.Polynomial([12,-7,1])
+        solver = newton.Newton(f, tol=1.e-15, maxiter=10,max_radius=100)
+        x0 = 90
+        x = solver.solve(x0)
+        self.assertEqual(x, 0) # 0 return value for max iteration error
+
+
+        
+
+    
 
 if __name__ == "__main__":
     unittest.main()
